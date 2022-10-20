df7bb7f658feb6258852fa914f04f7c3642540dd
Noryana Camargo <noryanacamargo@gmail.com>
2022-10-20 00:14:53 -0500

    Agrego boostrap

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCLOMbR2C-V1hLV2lFIi92jpZHcPkxneQ",
+  authDomain: "crud-web2-4d7af.firebaseapp.com",
+  projectId: "crud-web2-4d7af",
+  storageBucket: "crud-web2-4d7af.appspot.com",
+  messagingSenderId: "914933126170",
+  appId: "1:914933126170:web:857965f32e16c0a4b8eda3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
